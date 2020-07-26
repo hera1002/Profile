14d66fe680ff531b24b5c54ec63da1ed77d9de7a
hera1002 <hera1002>
2020-07-26 22:43:50 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>BBY LUV YOU</h1>
+        <h1> ---gds---Raja for you  </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > 
         </a>
       </header>
     </div>
