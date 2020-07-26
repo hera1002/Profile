73c4fe69b047a4be65aeee534a2d1f5d950ece71
hera1002 <hera1002>
2020-07-26 23:03:44 +0530

    remove icon

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <h1>BBY LUV YOU</h1>
         <h1> ---gds---Raja for you  </h1>
         <a
