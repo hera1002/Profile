b55c915d4e54843d85b46b9ee0cbafdfcf8adfac
Shanmugaraja A P <hera1002@gmail.com>
2020-07-26 22:51:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-    
         <h1>BBY LUV YOU</h1>
         <h1> ---gds---Raja for you  </h1>
         <a
